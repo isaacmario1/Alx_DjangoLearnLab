5ceee1944d3a4ad9fd93d11b1b8ca9168e5e7813
Isaac Mwangale <isaacmario.m1@gmail.com>
2025-04-19 09:13:43 +0300

    correct urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,3 @@
-# Create your views here.
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
@@ -13,4 +12,5 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
 
