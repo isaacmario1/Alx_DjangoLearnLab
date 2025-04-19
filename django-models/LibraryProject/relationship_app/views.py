5804d6a590898ebde29af187040ab335daf443f5
Isaac Mwangale <isaacmario.m1@gmail.com>
2025-04-19 08:11:40 +0300

    add urls in libraryProject and relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# List all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+# Detail of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
