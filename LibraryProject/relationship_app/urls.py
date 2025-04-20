11eaac1722d18a3094ce1af46ebdea5c36c10f03
Isaac Mwangale <isaacmario.m1@gmail.com>
2025-04-21 02:01:18 +0300

    Initial commit: Set up Django project with custom user model

---
+++ LibraryProject/relationship_app/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register
+from . import views
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
+]
