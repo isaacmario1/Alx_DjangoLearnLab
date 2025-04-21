31753e53f48c1765091073fb6634e1b0317e1dd2
Isaac Mwangale <isaacmario.m1@gmail.com>
2025-04-21 22:24:08 +0300

    add all files

---
+++ advanced_features_and_security/accounts/managers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
